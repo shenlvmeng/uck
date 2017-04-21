b45d235d460580194ef419990ffede3c0a2edd2a
xinym <xinym@rd.netease.com>
2017-04-21 13:44:35 +0800

    feat: add new component - icon for useful icon font
    This component has no props. See README.md for more information.

---
+++ lib/components/icon/Icon.vue
@@ -0,0 +1,37 @@
+<template>
+  <i
+    :class="classes"
+    :style="style"
+    :aria-hidden="aria"></i>
+</template>
+
+<script>
+  export default {
+    name: "Icon",
+    props: {
+      type: {
+        type: String,
+        required: true
+      },
+      color: String,
+      size: Number,
+      aria: Boolean
+    },
+    computed: {
+      classes () {
+        return `fa fa-${this.type}`
+      },
+      style () {
+        let styles = {};
+
+        if (this.size) {
+          styles['font-size'] = `${this.size}px`;
+        }
+
+        if (this.color) {
+          styles.color = this.color;
+        }
+      }
+    }
+  }
+</script>
