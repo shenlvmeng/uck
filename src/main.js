87b92a856ff40ccf6ef5eba72afef03776d36c24
xinym <xinym@rd.netease.com>
2017-05-08 19:40:50 +0800

    chore: divide some parts from main.js to App.vue

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
-import Icon from '../lib/components/icon/'
-import Form from '../lib/components/form/'
-import { Carousel, CarouselItem } from '../lib/components/carousel'
+import App from './App.vue'
 
 Vue.use(VueResource);
 
 new Vue({
   el: "#app",
-  data: {
-
-  },
-  components: {
-    icon: Icon,
-    carousel: Carousel,
-    phoneform: Form,
-    "carousel-item": CarouselItem
-  }
+  template: "<App/>",
+  components: { App }
 });
